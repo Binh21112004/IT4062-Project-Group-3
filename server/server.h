d8a5ad113eba5abebaf77940167d50ad76d1d77c
Đức Anh <MNDucAnh2809@gmail.com>
2025-12-31 10:58:17 +0700

    final_Nhom3

--- server/server.h
+++ server/server.h
@@ -21,8 +21,12 @@ void handle_register(ServerContext* ctx, int client_sock, char** fields, int fie
 void handle_login(ServerContext* ctx, int client_sock, char** fields, int field_count);
 void handle_logout(ServerContext* ctx, int client_sock, char** fields, int field_count);
 void handle_send_friend_request(ServerContext* ctx, int client_sock, char** fields, int field_count);
+void handle_accept_friend_request(ServerContext* ctx, int client_sock, char** fields, int field_count);
+void handle_reject_friend_request(ServerContext* ctx, int client_sock, char** fields, int field_count);
+void handle_unfriend(ServerContext* ctx, int client_sock, char** fields, int field_count);
 
 void handle_client_request(ServerContext* ctx, int client_sock, const char* buffer);
+
 void handle_create_event(ServerContext* ctx, int client_sock, char** fields, int field_count); // New
 void handle_get_events(ServerContext* ctx, int client_sock, char** fields, int field_count); // New
 void handle_get_event_detail(ServerContext* ctx, int client_sock, char** fields, int field_count); // Tuan 2
